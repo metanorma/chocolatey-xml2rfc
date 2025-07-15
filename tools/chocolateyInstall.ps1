cb861974b2ae82168d68260a7a00f2bd1c887b16
Ronald Tse <ronald.tse@ribose.com>
2025-07-15 13:55:19 +0800

    Fix chocolateyInstall.ps1 to use PyPI instead of non-existent GitHub release assets

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,11 +3,10 @@ param (
     [string]$pythonExe = "python"
 )
 
-$packageUrl = "https://github.com/ietf-tools/xml2rfc/releases/download/v${packageVersion}/xml2rfc-${packageVersion}.tar.gz"
 $venvDir = "${env:ChocolateyInstall}\lib\xml2rfc"
 
 & "$pythonExe" -m venv $venvDir
-& "$venvDir\Scripts\pip" install $packageUrl
+& "$venvDir\Scripts\pip" install "xml2rfc==$packageVersion"
 
 $executablePath = "$venvDir\Scripts\xml2rfc.exe"
 Install-BinFile -Name xml2rfc -Path $executablePath
